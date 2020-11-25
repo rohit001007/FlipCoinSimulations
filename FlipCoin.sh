fcba44d4a363ddb321449f6c10de478734a9d85e
rohit001007 <rohit001007@gmail.com>
2020-11-25 09:44:34 +0530

    Added : Loop Through Upto Multiple Times

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-r1=$(expr $RANDOM % 10)
-r2=$(expr $r1 % 2)
+Hcount=0
+Tcount=0
 
-if [ $r2 -eq 0 ]
-then
-	echo -ne "Head \n"
-else
-	echo -ne "Tail \n"
+for((i=0;i<21;i++))
+do
+	x1=$(expr $RANDOM % 10)
+	x2=$(expr $x1 % 2)
+
+
+	echo "Coin Tossed $i Times"
+	if [ $x2 -eq 0 ]
+	then
+		#echo -ne "Head \n"
+		Hcount=$(expr $Hcount + 1)
+
+	elif [ $x2 -eq 1 ]
+        then
+             #echo -ne "Tail \n"
+	  Tcount=$(expr $Tcount + 1)
 fi
+done
+
+echo -ne "\n * Head Comes $Hcount Times\n"
+echo -ne "\n * Tail Comes $Tcount Times\n"
 
